1c3b193e69c6bc15993a3e615b70f2c3d4334bda
Nuno Carvalho <nunoormaker21@gmail.com>
2023-01-23 20:29:01 +0000

    feat: add multisig capabilities

--- contracts/identity/OrgID.sol
+++ contracts/identity/OrgID.sol
@@ -11,16 +11,13 @@ contract OrgID {
      */
     mapping(address => bool) internal members;
     address owner;
-    uint public member_count;
+    uint256 public member_count;
 
     /**
      * @dev Modifier to ensure that only the owner can execute the function
      */
     modifier isOwner() {
-        require(
-            members[msg.sender] && msg.sender == owner,
-            "OrgID: Not owner"
-        );
+        require(members[msg.sender] && msg.sender == owner, "OrgID: Not owner");
         _;
     }
 
@@ -28,10 +25,7 @@ contract OrgID {
      * @dev Modifier to ensure that only a signer can execute the function
      */
     modifier isMember() {
-        require(
-            members[msg.sender],
-            "OrgID: Not signer"
-        );
+        require(members[msg.sender], "OrgID: Not signer");
         _;
     }
 
@@ -39,32 +33,30 @@ contract OrgID {
      * @dev Events
      */
 
-    event OrganizationCreated(
-        address indexed _owner,
-        address[] _signers
-    );
-
-    event MemberAdded(
-        address indexed _member
-    );
-
-    event MemberRemoved(
-        address indexed _member
-    );
-
-    event OwnershipTransferred(
-        address indexed _from,
-        address indexed _to
+    event OrganizationCreated(address indexed _owner, address[] _signers);
+    event MemberAdded(address indexed _member);
+    event MemberRemoved(address indexed _member);
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+    event ExecutedTransaction(
+        address indexed _to,
+        uint256 indexed _value,
+        bytes _data
     );
 
     /**
      * @dev Constructor to initialize the master wallet index
      */
     constructor(address _owner, address[] memory _signers) {
-        require(_owner != address(0x0) || _owner != address(this), "OrgID: Invalid owner address");
-        
+        require(
+            _owner != address(0x0) || _owner != address(this),
+            "OrgID: Invalid owner address"
+        );
+
         for (uint256 i = 0; i < _signers.length; i++) {
-            require(_signers[i] != address(0x0) || _signers[i] != address(this), "OrgID: Invalid signer address");
+            require(
+                _signers[i] != address(0x0) || _signers[i] != address(this),
+                "OrgID: Invalid signer address"
+            );
             members[_signers[i]] = true;
             member_count++;
         }
@@ -83,10 +75,7 @@ contract OrgID {
      * @dev Function to add a wallet to the mapping
      * @param _wallet The address of the wallet
      */
-    function addMember(address _wallet)
-        public
-        isOwner
-    {
+    function addMember(address _wallet) public isOwner {
         require(!members[_wallet], "Wallet already exists");
         members[_wallet] = true;
         member_count++;
@@ -111,7 +100,10 @@ contract OrgID {
      * @param _newOwner The address of the new master
      */
     function transferOwnership(address _newOwner) public isOwner {
-        require(_newOwner != address(0), "OrgID: New owner address cannot be 0x0");
+        require(
+            _newOwner != address(0),
+            "OrgID: New owner address cannot be 0x0"
+        );
 
         // Add new owner to members list
         if (!members[_newOwner]) {
@@ -122,5 +114,19 @@ contract OrgID {
         // Transfer ownership
         emit OwnershipTransferred(owner, _newOwner);
         owner = _newOwner;
+    }
+
+    function execTransaction(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) public payable virtual isMember returns (bool success) {
+        require(_to != address(0), "OrgID: Cannot send to address 0x0");
+
+        (success, ) = _to.call{value: _value}(
+            _data
+        );
+
+        if (success) emit ExecutedTransaction(_to, _value, _data);
     }
 }
