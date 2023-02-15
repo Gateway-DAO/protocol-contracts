31d3b89458fcd2877c9f8e71146a2b3d12470ef9
Rafa <rafaelgomes.web@gmail.com>
2023-02-15 16:49:26 -0300

    Added overriden methods and the tokenURI feature
    Co-authored-by: Nuno Carvalho <NunoMartins21@users.noreply.github.com>

--- contracts/CredentialNFT.sol
+++ contracts/CredentialNFT.sol
@@ -2,22 +2,21 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract CredentialNFT is ERC721, Ownable {
+contract CredentialNFT is ERC721, ERC721URIStorage, Ownable {
     mapping (string => address) private credentialToMinter;
     mapping (string => bytes) private credentialToMetadataSig;
-    mapping (address => bool) private whitelist;
 
     event CredentialMinted(string indexed credentialId, address indexed minter, uint256 indexed tokenId);
     event MinterRemoved(string indexed credentialId, address indexed minter);
-    event CredentialRegistered(string indexed credentialId, address indexed minter);
+    event MinterRegistered(string indexed credentialId, address indexed minter);
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
 
-    function setMinter(string memory _credentialId, address _minter) public onlyOwner {
-        require(_minter != address(0), "CredentialNFT: Minter cannot be the zero address");
+    function setMinter(string memory _credentialId, address _minter) internal onlyOwner {
         credentialToMinter[_credentialId] = _minter;
     }
 
@@ -28,54 +27,53 @@ contract CredentialNFT is ERC721, Ownable {
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) override internal {
-        require(from == address(0) || to == address(0), "This a Soulbound token. It cannot be transferred. It can only be burned by the token owner.");
+        require(from == address(0) || to == address(0), "CredentialNFT: You cannot transfer the NFT to other accounts.");
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    function registerCredential(string memory _credentialId, string memory _metadataUrl, bytes memory _metadataSig) external {
-        // Verify the metadata signature
-        (address recovered, ) = ECDSA.tryRecover(
-            ECDSA.toEthSignedMessageHash(bytes(_metadataUrl)),
-            _metadataSig
-        );
-        require(recovered == msg.sender, "CredentialNFT: Invalid metadata signature");
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function registerCredential(string memory _credentialId, address _recipient) external onlyOwner {
+        require(_recipient != address(0), "CredentialNFT: Recipient cannot be the zero address");
 
         // Register the credential and authorize the caller to mint NFTs for it
-        setMinter(_credentialId, msg.sender);
-        credentialToMetadataSig[_credentialId] = _metadataSig; // added
-        emit CredentialRegistered(_credentialId, msg.sender);
+        setMinter(_credentialId, _recipient);
+        emit MinterRegistered(_credentialId, _recipient);
     }
 
-    function addToWhitelist(address _address) external onlyOwner {
-        require(_address != address(0), "CredentialNFT: Address cannot be the zero address");
-        whitelist[_address] = true;
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+         if (bytes(tokenURI(tokenId)).length != 0) {
+            _setTokenURI(tokenId, "");
+        }
     }
 
-    function isValid(string memory _credentialId) public view returns (bool) {
-        // Get the metadata signature from the CredentialNFT contract
-        bytes memory metadataSig = credentialToMetadataSig[_credentialId];
-
+    function isValid(string memory _tokenURI, bytes memory _metadataSig) public view returns (bool) {
         // Recover the signer from the metadata signature
-        string memory metadataUrl = string(abi.encodePacked("metadata url for ", _credentialId));
         (address recovered, ) = ECDSA.tryRecover(
-            ECDSA.toEthSignedMessageHash(bytes(metadataUrl)),
-            metadataSig
+            ECDSA.toEthSignedMessageHash(bytes(_tokenURI)),
+            _metadataSig
         );
 
         // Ensure the signer is the owner of the CredentialNFT contract
         return recovered == owner();
     }
 
-    function mintNFT(string memory _credentialId) external {
-        require(whitelist[msg.sender], "CredentialNFT: Only whitelisted addresses can mint NFTs");
+    function mintNFT(string memory _credentialId, string memory _tokenURI, bytes memory _metadataSig) external {
         require(credentialToMinter[_credentialId] == msg.sender, "CredentialNFT: Only the registered minter can mint NFTs for this credential");
 
         // Ensure the credential is valid
-        require(isValid(_credentialId), "CredentialNFT: Invalid credential");
+        require(isValid(_tokenURI, _metadataSig), "CredentialNFT: Invalid metadata");
 
         // Mint the NFT and transfer it to the minter
         uint256 tokenId = uint256(keccak256(abi.encodePacked(_credentialId, msg.sender, block.timestamp)));
+
         _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+
         emit CredentialMinted(_credentialId, msg.sender, tokenId);
     }
+    
 }
