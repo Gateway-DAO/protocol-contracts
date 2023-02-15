82d98a2172d9d49a3c130ff817a63e1ae60853e2
Rafa <rafaelgomes.web@gmail.com>
2023-02-15 20:23:39 -0300

    Added a few optimizers in the hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,24 @@ import "hardhat-deploy";
 import { getDeployerAccount, getNodeURL } from "./utils/network";
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.17",
+    solidity: {
+        version: "0.8.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+            outputSelection: {
+                "*": {
+                    "*": ["metadata", "evm.bytecode"],
+                    "": ["ast"],
+                },
+            },
+            metadata: {
+                bytecodeHash: "none",
+            },
+        },
+    },
     namedAccounts: {
         deployer: {
             default: getDeployerAccount(),
