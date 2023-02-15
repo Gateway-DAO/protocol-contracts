29f40c3f363d109e1cb37395be301f356b8b770f
Rafa <rafaelgomes.web@gmail.com>
2023-02-14 22:32:45 -0300

    prettier fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import 'hardhat-deploy';
+import "hardhat-deploy";
 import { getDeployerAccount, getNodeURL } from "./utils/network";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-  namedAccounts: {
-    deployer: {
-      default: getDeployerAccount()
-    }
-  },
-  networks: {
-    goerli: {
-      url: getNodeURL('goerli'),
-      accounts: [process.env.PRIVATE_KEY as string]
+    solidity: "0.8.17",
+    namedAccounts: {
+        deployer: {
+            default: getDeployerAccount(),
+        },
     },
-    polygon: {
-      url: getNodeURL('polygon'),
-      accounts: [process.env.PRIVATE_KEY as string]
+    networks: {
+        goerli: {
+            url: getNodeURL("goerli"),
+            accounts: [process.env.PRIVATE_KEY as string],
+        },
+        polygon: {
+            url: getNodeURL("polygon"),
+            accounts: [process.env.PRIVATE_KEY as string],
+        },
+        mumbai: {
+            url: getNodeURL("mumbai"),
+            accounts: [process.env.PRIVATE_KEY as string],
+        },
     },
-    mumbai: {
-      url: getNodeURL('mumbai'),
-      accounts: [process.env.PRIVATE_KEY as string]
-    },
-  }
 };
 
 export default config;
