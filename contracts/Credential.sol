49584f60a984a8e962224fce57afe6df1f0f89f9
Nuno Carvalho <nunoormaker21@gmail.com>
2023-02-28 00:02:00 +0000

    feat: implemented credential updates; created credentialExists modifier

--- contracts/Credential.sol
+++ contracts/Credential.sol
@@ -68,6 +68,7 @@ contract CredentialContract is Ownable {
         address[] permissions
     );
 
+    event CredentialUpdated(string id, string url, bytes metadata_sig);
     event CredentialRevoked(string id);
     event CredentialSuspended(string id);
     event CredentialReactivated(string id);
@@ -81,6 +82,14 @@ contract CredentialContract is Ownable {
     /**
      * @dev Modifiers
      */
+    modifier credentialExists(string memory _id) {
+        require(
+            keccak256(bytes(credentials[_id].id)) == keccak256(bytes(_id)),
+            "Credential: Credential does not exist"
+        );
+        _;
+    }
+    
     modifier onlyIssuer(string memory _id) {
         require(
             msg.sender == credentials[_id].issuer,
@@ -150,7 +159,49 @@ contract CredentialContract is Ownable {
         credentials[_id] = newCredential;
     }
 
-    function isValid(string memory _id) public view returns (bool) {
+    function updateCredential(
+        string memory _id,
+        string memory _url,
+        string memory _name,
+        string memory _description,
+        string memory _revoked_conditions,
+        string memory _suspended_conditions,
+        bytes memory _metadata_sig
+    ) public credentialExists(_id) onlyIssuer(_id) {
+        Credential storage c = credentials[_id];
+
+        if(bytes(_url).length > 0 && keccak256(bytes(c.metadata_url)) != keccak256(bytes(_url))){
+            c.metadata_url = _url;
+        }
+        
+        if(bytes(_name).length > 0 && keccak256(bytes(c.context.name)) != keccak256(bytes(_name))){
+            c.context.name = _name;
+        }
+        
+        if(bytes(_description).length > 0 && keccak256(bytes(c.context.description)) != keccak256(bytes(_description))){
+            c.context.description = _description;
+        }
+        
+        if(bytes(_revoked_conditions).length > 0 && keccak256(bytes(c.context.revoked_conditions)) != keccak256(bytes(_revoked_conditions))){
+            c.context.revoked_conditions = _revoked_conditions;
+        }
+        
+        if(bytes(_suspended_conditions).length > 0 && keccak256(bytes(c.context.suspended_conditions)) != keccak256(bytes(_suspended_conditions))){
+            c.context.suspended_conditions = _suspended_conditions;
+        }
+        
+        if(_metadata_sig.length > 0 && keccak256(c.metadata_sig) != keccak256(_metadata_sig)){
+            c.metadata_sig = _metadata_sig;
+        }
+
+        emit CredentialUpdated(
+            _id,
+            c.metadata_url,
+            c.metadata_sig
+        );
+    }
+
+    function isValid(string memory _id) public credentialExists(_id) view returns (bool) {
         bool status = credentials[_id].status == CredentialStatus.Active
             ? true
             : false;
@@ -166,7 +217,7 @@ contract CredentialContract is Ownable {
         return recovered == credentials[_id].issuer ? true : false;
     }
 
-    function reactivateCredential(string memory _id) public onlyIssuerOrAuthorized(_id) {
+    function reactivateCredential(string memory _id) public credentialExists(_id) onlyIssuerOrAuthorized(_id) {
         require(
             credentials[_id].status == CredentialStatus.Suspended,
             "Credential: Credential is not suspended"
@@ -176,7 +227,7 @@ contract CredentialContract is Ownable {
         emit CredentialReactivated(_id);
     }
 
-    function revokeCredential(string memory _id) public onlyIssuerOrAuthorized(_id) {
+    function revokeCredential(string memory _id) public credentialExists(_id) onlyIssuerOrAuthorized(_id) {
         require(
             credentials[_id].status == CredentialStatus.Active,
             "Credential: Credential is not active");
@@ -185,7 +236,7 @@ contract CredentialContract is Ownable {
         emit CredentialRevoked(_id);
     }
 
-    function suspendCredential(string memory _id) public onlyIssuerOrAuthorized(_id) {
+    function suspendCredential(string memory _id) public credentialExists(_id) onlyIssuerOrAuthorized(_id) {
         require(
             credentials[_id].status == CredentialStatus.Active,
             "Credential: Credential is not active"
